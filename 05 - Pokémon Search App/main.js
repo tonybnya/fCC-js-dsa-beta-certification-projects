503cc4b12a2d5928ddf980a14167e20079f98dba
Tony <nya.tony2010@gmail.com>
2024-04-22 01:54:08 +0100

    fix: the script and successfully submit the project to fCC

--- 05 - Pokémon Search App/main.js
+++ 05 - Pokémon Search App/main.js
@@ -1,25 +1,74 @@
-// const searchInput = document.getElementById("search-input");
-// const searchBtn = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-button");
 const allPokemonsUrl = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
 
 const formatInput = (input) => {
   // lowercase the input
-  let string = input.toLowerCase();
+  let endpoint = input.toLowerCase();
 
   // Replace special characters for gender
   // \u2640: Unicode character for the female symbol (♀)
   // \u2642: Unicode character for the male symbol (♂)
-  string = string.replace(/[\u2640\u2642]/g, (match) => {
+  endpoint = endpoint.replace(/[\u2640\u2642]/g, (match) => {
     return match === "\u2640" ? "f" : "m";
   });
   
   // remove special characters and replace empty spaces by '-'
-  string = string.replace(/[^a-z0-9\s]/g, '').replace(/\s/g, '-');
+  endpoint = endpoint.replace(/[^a-z0-9\s]/g, '').replace(/\s/g, '-');
 
-  return string;
+  return endpoint;
 };
 
-console.log(formatInput("Pikachu")); // pikachu
-console.log(formatInput("Nirodan ♀")); // nirodan-f
-console.log(formatInput("Nirodan ♂")); // nirodan-m
-console.log(formatInput("Mr. Mime")); // mr-mime
+const fetchData = async (endpoint) => {
+  try {
+    const url = `${allPokemonsUrl}/${endpoint}`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    console.log(data);
+
+    const pokemonDescription = document.getElementById('pokemon-description');
+    const identification = document.getElementById('identification');
+    // identification.innerHTML = `<span id="pokemon-name">${data.name.toUpperCase()}</span> #<span id="pokemon-id">${data.id}</span>`;
+
+    document.getElementById('pokemon-name').textContent = data.name.toUpperCase();
+    document.getElementById('pokemon-id').textContent = data.id;
+
+    // Construct biometric paragraph
+    const biometric = document.getElementById('biometric');
+    biometric.textContent = `Weight: ${data.weight} Height: ${data.height}`;
+
+    // Construct types
+    const typesDiv = document.getElementById('types');
+    typesDiv.innerHTML = data.types.map(type => `<span>${type.type.name.toUpperCase()}</span>`).join(' ');
+
+    // Set image source
+    const img = pokemonDescription.querySelector('img');
+    img.src = data.sprites.front_default;
+    img.alt = data.name;
+    img.setAttribute('id', 'sprite');
+
+    const statsTable = document.querySelector('table');
+    const statIds = ["hp", "attack", "defense", "special-attack", "special-defense", "speed"];
+
+    data.stats.forEach(stat => {
+      const statName = stat.stat.name;
+      const statId = statIds.find(id => id.includes(statName.toLowerCase()));
+      if (statId) {
+        const statCell = document.getElementById(statId);
+        statCell.textContent = stat.base_stat;
+      }
+    });
+
+  } catch(err) {
+    alert("Pokémon not found");
+  }
+};
+
+searchBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  const endpoint = formatInput(searchInput.value);
+  console.log(endpoint);
+  fetchData(endpoint);
+  searchInput.value = "";
+})
